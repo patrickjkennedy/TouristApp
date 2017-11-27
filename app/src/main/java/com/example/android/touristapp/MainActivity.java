5b5c4020710b9403cf4ed13a84b68f73a0276272
pkennedy89 <patrickken@gmail.com>
2017-11-27 19:55:28 +0000

    added empty activities and tested main

--- app/src/main/java/com/example/android/touristapp/MainActivity.java
+++ app/src/main/java/com/example/android/touristapp/MainActivity.java
@@ -1,13 +1,101 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.android.touristapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
+
+import com.example.android.touristapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
+
+        // Find the View that shows the culture category
+        TextView numbers = (TextView) findViewById(R.id.culture);
+
+        // Set a click listener on that View
+        numbers.setOnClickListener(new OnClickListener() {
+            // The code in this method will be executed when the numbers category is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link NumbersActivity}
+                Intent numbersIntent = new Intent(MainActivity.this, CultureActivity.class);
+
+                // Start the new activity
+                startActivity(numbersIntent);
+            }
+        });
+
+        // Find the View that shows the restaurants category
+        TextView family = (TextView) findViewById(R.id.restaurants);
+
+        // Set a click listener on that View
+        family.setOnClickListener(new OnClickListener() {
+            // The code in this method will be executed when the family category is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link FamilyActivity}
+                Intent restaurantsIntent = new Intent(MainActivity.this, RestaurantsActivity.class);
+
+                // Start the new activity
+                startActivity(restaurantsIntent);
+            }
+        });
+
+        // Find the View that shows the pubs category
+        TextView colors = (TextView) findViewById(R.id.pubs);
+
+        // Set a click listener on that View
+        colors.setOnClickListener(new OnClickListener() {
+            // The code in this method will be executed when the colors category is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link ColorsActivity}
+                Intent pubsIntent = new Intent(MainActivity.this, PubsActivity.class);
+
+                // Start the new activity
+                startActivity(pubsIntent);
+            }
+        });
+
+        // Find the View that shows the outdoor activities category
+        TextView phrases = (TextView) findViewById(R.id.outdoor_activities);
+
+        // Set a click listener on that View
+        phrases.setOnClickListener(new OnClickListener() {
+            // The code in this method will be executed when the phrases category is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link PhrasesActivity}
+                Intent outdoorIntent = new Intent(MainActivity.this, OutdoorActivity.class);
+
+                // Start the new activity
+                startActivity(outdoorIntent);
+            }
+        });
     }
 }
